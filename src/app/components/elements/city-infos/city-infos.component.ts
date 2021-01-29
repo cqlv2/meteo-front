5fb4d16342fb2e4aada32d7c49222fc21c3f957a
sylvain amianto <samianto@gmail.com>
2021-01-29 18:06:48 +0100

    premier graph fonctionel

--- src/app/components/elements/city-infos/city-infos.component.ts
+++ src/app/components/elements/city-infos/city-infos.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { City } from 'src/app/models/city';
+import { Weather } from 'src/app/models/weather';
 import { CityService } from 'src/app/services/city.service';
 
 @Component({
@@ -11,12 +12,55 @@ export class CityInfosComponent implements OnInit {
 
   city: City;
 
+  statMonth: number;
+  statYear: number;
+
+  temp: number[]
+  tempByMonthByhour: number[]
+  tempForDays: Weather[]=[];
+
+
+
+month:number =new Date().getMonth();
+year:number =new Date().getFullYear();
+
+  maptempByMonth = new Map<number, [Date,number[]]>();
+
+
   constructor(private citySrv: CityService) { }
 
   ngOnInit(): void {
     this.citySrv.getFromCitySubject().subscribe(
-      data => this.city = data,
-      err => console.log(err)     
+      data => {
+        this.city = data
+        data.weathers.forEach(weather => {
+          // recuperation des temperature pour un mois,  par entre
+          // on veut recup toutes les valeurs pour janvier
+          //maptempByMonth<"1", avg([1,2,3,4,5,6,7,8,...]) >
+          
+          let test=new Date(weather.dateAdd);
+          if (test.getMonth() == this.month && test.getFullYear() == this.year) 
+            //on a une entre de weather correspondant a janvier 2021
+            this.tempForDays.push(weather);
+        });
+        
+        this.tempForDays.forEach(day => {
+              let test=new Date(day.dateAdd);
+              if (this.maptempByMonth.has(test.getDate()))
+                this.maptempByMonth.get(test.getDate())[1].push(day.temp)
+              else {
+                this.maptempByMonth.set(test.getDate(), [day.dateAdd,[day.temp]])
+                
+              }
+            });
+            // this.tempByMonthByhour.push(weather.temp)
+          
+          // this.tempByMonthByhour.reduce((a, b) => a + b) / this.tempByMonthByhour.length
+      
+        console.log(this.maptempByMonth);
+
+      },
+      err => console.log(err)
     );
 
   }
--- src/app/components/elements/widget-weather-stat/widget-weather-stat.component.ts
+++ src/app/components/elements/widget-weather-stat/widget-weather-stat.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import { Weather } from 'src/app/models/weather';
 
 @Component({
   selector: 'app-widget-weather-stat',
@@ -9,89 +10,96 @@ import { Color, Label } from 'ng2-charts';
 })
 export class WidgetWeatherStatComponent implements OnInit {
 
-  // public lineChartData: ChartDataSets[] = [
-  //   { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-  //   { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
-  // ];
-  // public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  // public lineChartOptions: (ChartOptions & { annotation: any }) = {
-  //   responsive: true,
-  //   scales: {
-  //     // We use this empty structure as a placeholder for dynamic theming.
-  //     xAxes: [{}],
-  //     yAxes: [
-  //       {
-  //         id: 'y-axis-0',
-  //         position: 'left',
-  //       },
-  //       {
-  //         id: 'y-axis-1',
-  //         position: 'right',
-  //         gridLines: {
-  //           color: 'rgba(255,0,0,0.3)',
-  //         },
-  //         ticks: {
-  //           fontColor: 'red',
-  //         }
-  //       }
-  //     ]
-  //   },
-  //   annotation: {
-  //     annotations: [
-  //       {
-  //         type: 'line',
-  //         mode: 'vertical',
-  //         scaleID: 'x-axis-0',
-  //         value: 'March',
-  //         borderColor: 'orange',
-  //         borderWidth: 2,
-  //         label: {
-  //           enabled: true,
-  //           fontColor: 'orange',
-  //           content: 'LineAnno'
-  //         }
-  //       },
-  //     ],
-  //   },
-  // };
-  // public lineChartColors: Color[] = [
-  //   { // grey
-  //     backgroundColor: 'rgba(148,159,177,0.2)',
-  //     borderColor: 'rgba(148,159,177,1)',
-  //     pointBackgroundColor: 'rgba(148,159,177,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  //   },
-  //   { // dark grey
-  //     backgroundColor: 'rgba(77,83,96,0.2)',
-  //     borderColor: 'rgba(77,83,96,1)',
-  //     pointBackgroundColor: 'rgba(77,83,96,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(77,83,96,1)'
-  //   },
-  //   { // red
-  //     backgroundColor: 'rgba(255,0,0,0.3)',
-  //     borderColor: 'red',
-  //     pointBackgroundColor: 'rgba(148,159,177,1)',
-  //     pointBorderColor: '#fff',
-  //     pointHoverBackgroundColor: '#fff',
-  //     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-  //   }
-  // ];
-  // public lineChartLegend = true;
-  // public lineChartType: ChartType = 'line';
-  // // public lineChartPlugins = [pluginAnnotations];
+@Input() maptempByMonth:Map<number,[Date, number[]]>;
 
-  // // @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
+// wearherToShow:Weather[]
+
+
+temp:number[]=[];
+lbl:string[]=[];
+
+
+  // valeur a afficher
+  public lineChartData: ChartDataSets[] = [
+    { data: this.temp, label: 'temp' },
+    // { data: [28, 48, 40, 19, 86, 27, 30, 120], label: 'Series B' },
+    // { data: [180, 480, 770, 90, 300, 270, 400], label: 'Series C',yAxisID: 'y-axis-1' }
+  ];
+  //public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'aze'];
+
+  public lineChartLabels: Label[] = this.lbl;
+
+ 
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{}],
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        },
+        {
+          id: 'y-axis-1',
+          position: 'right',
+          gridLines: {
+            color: 'green',
+          },
+          ticks: {
+            fontColor: 'blue',
+          }
+        }
+      ]
+    },
+    annotation: {},
+  };
+  public lineChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // red
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend = true;
+  public lineChartType: ChartType = 'line';
+  // public lineChartPlugins = [pluginAnnotations];
+
+  // @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
 
 
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit(): void {
+   this.maptempByMonth.forEach(day => {
+     let test=new Date(day[0]);
+    this.temp.push(day[1].reduce((a, b) => a + b) / day[1].length);
+    this.lbl.push(test.getDate()+"/"+test.getMonth()+"/"+test.getFullYear());
+  });
+
+
+
   }
 
 
